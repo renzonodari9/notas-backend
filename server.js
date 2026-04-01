6766f3fb1b811e9bcc38a73eb75fc2d8904f570f
Renzo Nodari <renzonodari9@gmail.com>
2026-04-01 12:14:40 -0300

    fix render pport

--- server.js
+++ server.js
@@ -15,4 +15,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(5000, () => console.log("Server corriendo"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server corriendo en puerto " + PORT);
+});
